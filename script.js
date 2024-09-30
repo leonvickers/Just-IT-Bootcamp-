b318e7aeb8a7151e2eabe35371394cc410a92645
leonvickers <leon.vickers@btinternet.com>
2024-09-30 11:44:44 +0100

    SImple Image Slider using JavaScript
    Simple image slider using JavaScript and rendered using HTML and CSS

--- script.js
+++ script.js
@@ -1,27 +1,82 @@
-// Set the date we're counting down to
-let countDownDate = new Date("december 25, 2024 15:37:25").getTime();
+// local reviews data
+const reviews = [
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: "anna johnson",
+    job: "web designer",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "peter jones",
+    job: "intern",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+];
 
-// Update the count down every 1 second
-let x = setInterval(function () {
-  // Get today's date and time
-  let now = new Date().getTime();
+// select items
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
 
-  // Find the distance between now and the count down date
-  let distance = countDownDate - now;
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
 
-  // Time calculations for days, hours, minutes and seconds
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+// set starting item
+let currentItem = 0;
 
-  // Output the result in an element with id="demo"
-  document.getElementById("countDown").innerHTML =
-    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+// load initial items
+window.addEventListener("DOMContentLoaded", function (e) {
+  showPerson();
+});
 
-  // If the count down is over, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countDown").innerHTML = "Complete";
+// show person based on items
+function showPerson() {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+// show next showPerson
+nextBtn.addEventListener("click", function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
   }
-}, 1000);
+  showPerson();
+});
+
+// show prev showPerson
+prevBtn.addEventListener("click", function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson();
+});
+
+// show random showPerson
+randomBtn.addEventListener("click", function (e) {
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson();
+});
